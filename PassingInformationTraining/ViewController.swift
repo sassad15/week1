ba43fffb0f0bd947c084a988e5c9c31d6bf80813
Shehzeb <msait0601@gmail.com>
2019-08-09 08:35:20 -0400

    week 1

---
+++ PassingInformationTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassingInformationTraining
+//
+//  Created by Raja Zeb on 8/5/19.
+//  Copyright © 2019 Muhammad Assad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
